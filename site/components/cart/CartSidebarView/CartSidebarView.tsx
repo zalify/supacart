db8e8c2bb8fcfb6ce70af3a3bd68489628efe136
Ryan <962491243@qq.com>
2022-11-04 18:30:24 +0800

    show discount

--- site/components/cart/CartSidebarView/CartSidebarView.tsx
+++ site/components/cart/CartSidebarView/CartSidebarView.tsx
@@ -25,11 +25,26 @@ const CartSidebarView: FC = observer(() => {
   const isEmpty = gm ? gm.isCartEmpty : cartData.isEmpty
   const isLoading = gm ? false : cartData.isLoading
 
-  const totalPrice = gm ? gm.totalPrice : cartData.data?.totalPrice
+  const totalPrice = (gm ? gm.totalPrice : cartData.data?.totalPrice) || 0
 
+  const discountPrice = totalPrice >= 100 ? totalPrice * 0.2 : 0
+
+  const { price: subTotal } = usePrice(
+    cartData.data && {
+      amount: totalPrice,
+      currencyCode: cartData.data.currency.code,
+    }
+  )
+
+  const { price: discount } = usePrice(
+    cartData.data && {
+      amount: discountPrice,
+      currencyCode: cartData.data.currency.code,
+    }
+  )
   const { price: total } = usePrice(
     cartData.data && {
-      amount: Number(totalPrice),
+      amount: discountPrice > 0 ? totalPrice * 0.8 : Number(totalPrice),
       currencyCode: cartData.data.currency.code,
     }
   )
@@ -210,20 +225,18 @@ const CartSidebarView: FC = observer(() => {
           </div>
 
           <div className="flex-shrink-0 px-6 py-6 sm:px-6 sticky z-20 bottom-0 w-full right-0 left-0 bg-accent-0 border-t text-sm">
-            {/* <ul className="pb-2">
-              <li className="flex justify-between py-1">
-                <span>Subtotal</span>
-                <span>{subTotal}</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Taxes</span>
-                <span>Calculated at checkout</span>
-              </li>
-              <li className="flex justify-between py-1">
-                <span>Shipping</span>
-                <span className="font-bold tracking-wide">FREE</span>
-              </li>
-            </ul> */}
+            {discountPrice > 0 && (
+              <ul className="pb-2">
+                <li className="flex justify-between py-1">
+                  <span>小计</span>
+                  <span>{subTotal}</span>
+                </li>
+                <li className="flex justify-between py-1">
+                  <span>折扣</span>
+                  <span>{discount}</span>
+                </li>
+              </ul>
+            )}
             <div className="flex justify-between py-3 font-bold mb-2">
               <span>总计</span>
               <span>{total}</span>
